9176276f389583d295be458c923e95bff1f9648f
hasagiiiiii <dvant24102003@gmail.com>
2025-03-22 20:04:11 +0700

    update --version

--- src/Store/saga.ts
+++
@@ -1,8 +0,0 @@
-import { all } from "redux-saga/effects";
-import { LayoutSaga } from "../Common/Layout/store/layout.store.saga";
-
-export function* rootSaga() {
-    yield all([
-        LayoutSaga()
-    ]);
-}
